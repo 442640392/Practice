362a82a352a1b6fb7ed5cefa0a49ba724685cb20
nixiahui <znxhsweet@sina.com>
2016-10-24 14:03:15 +0800

    Initial Commit

---
+++ Load AnimationTests/Load_AnimationTests.swift
@@ -0,0 +1,36 @@
+//
+//  Load_AnimationTests.swift
+//  Load AnimationTests
+//
+//  Created by Nao on 16/10/24.
+//  Copyright © 2016年 倪夏辉. All rights reserved.
+//
+
+import XCTest
+@testable import Load_Animation
+
+class Load_AnimationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
